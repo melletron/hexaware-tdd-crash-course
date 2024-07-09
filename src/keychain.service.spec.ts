bab89950c022cfc8553b1b5ec59dc6a68e89b49f
Melle Hofman <mhofman@mobiquityinc.com>
2024-07-09 12:55:21 +0200

    Add the main scaffolding for initialization of the keychain
    The keychain is created so that it stores the public and private key in local storage.
    If there is already a key in local storage, it tries to load that first, but if it fails
    it will overwrite them with newly generate keys.
    
    We now need to verify the proper functioning of the keys by creating the specification for encrypt and decrypt.
    
    I had to add a ready function because the used Crypto API is async, and constructor methods are not allowed to be async.

--- src/keychain.service.spec.ts
+++ src/keychain.service.spec.ts
@@ -1,10 +1,106 @@
 import {expect} from "chai";
+
 import {localStorage} from "./mocks/local-storage.mock";
 import {KeychainService} from "./keychain.service";
 
-let keyChainService: KeychainService;
-const textEncoder = new TextEncoder();
-const textDecoder = new TextDecoder();
+const sinon = require("sinon");
+
+// We are having 2 stub keys as values we can use for our test
+// These are generated manually using the Crypto library
+const STUB_PRIVATE_KEY = JSON.stringify({
+    "alg": "RSA-OAEP-256",
+    "d": "B15ihGqNnRwmGjIIOSz4q6eZ4_2FrDnJhVUT-MtwDUmN4wXeSn1c0v_0jK1oUDzmvZ0RqgDkHuKV4QHFXr6oxEa4lqO7oAySmeC2ArJ7jtM1GfBUTRymR3twHfghKb4A2_2fqNXi7G3-H6UD6ZptkTLoqg2A82wXWooijel8gQ",
+    "dp": "c2N9lZgzlmsf_gsZwXAN7OP83a6lsn3yg3VeX4nUvgKhrBYwrLAYJSY0Zfz30jK1zeW7RFqK4kWqDpaR_jMXAQ",
+    "dq": "iX10_IGlBE62OappS6qILIt7gHGShxsABAmFx-3VMUevGUWJB-6gRtYKLppS6SknO7EtVCbQjrPU0vD5ux1mjQ",
+    "e": "AQAB",
+    "ext": true,
+    "key_ops": ["decrypt"],
+    "kty": "RSA",
+    "n": "tRiARNa5Dj_L34A7caaTxah35ZNcEmFR1eGpRI0jJEAMeK40xTb5YPiIYXfGbhGQ4aVOgMHeL79u79ag0UhmsDoyZgoCxs4lJuAxZ4YGFqGLisbNNqdQFTJe1UG0EK12kDd7yXzhKDCE9KNWOqU61bhw5AocdAP6uATdYJD5Qn0",
+    "p": "4waSH256jkrnpXWRw9pOVi7k8ea5x54I1Wgex3u4ra9NExL1P0mONFcSZnjT_LMJ41hn-90iHS2LzIsj_SY8gQ",
+    "q": "zDVME9V4hLfV85OSYhUg_Sk1sb3F_6A-X9F1LHq5W_HSpdnfe_OKBm4cTe5t_FJ0vygi4zT9tJ1uoxih0k_3_Q",
+    "qi": "SKkN5l5LaDJpKqvZM5Mai3GStECD36A2fKkEStFmjKomwqHepWwk3XboqmwrCSX9Nump4eb2M2LxxdnBCzjyuA"
+});
+const STUB_PUBLIC_KEY = JSON.stringify({
+    "alg": "RSA-OAEP-256",
+    "e": "AQAB",
+    "ext": true,
+    "key_ops": ["encrypt"],
+    "kty": "RSA",
+    "n": "tRiARNa5Dj_L34A7caaTxah35ZNcEmFR1eGpRI0jJEAMeK40xTb5YPiIYXfGbhGQ4aVOgMHeL79u79ag0UhmsDoyZgoCxs4lJuAxZ4YGFqGLisbNNqdQFTJe1UG0EK12kDd7yXzhKDCE9KNWOqU61bhw5AocdAP6uATdYJD5Qn0"
+});
 
 describe('KeyChain', () => {
+
+    describe('constructor / initialize / ready', () => {
+
+        it('takes previously generated keys from localStorage and uses that as the key pair', async () => {
+
+            //localStorage is a singleton, so let's make sure no keys are stored there
+            localStorage.removeItem('vault-public-key');
+            localStorage.removeItem('vault-private-key');
+
+            // We store our stub keys into local storage
+            localStorage.setItem('vault-public-key', STUB_PUBLIC_KEY);
+            localStorage.setItem('vault-private-key', STUB_PRIVATE_KEY);
+
+            const keyChain = new KeychainService();
+            // All crypto operations are async, because they're computationally heavy and thus offloaded to a web worker
+            // Therefore we also need to create a ready method
+            await keyChain.ready();
+
+            // Crypto, that's meant to be with a lot of private parts
+            // So the only thing we can actually validate, is the n value
+            // We need to verify the actual functioning of it through other methods
+            expect(keyChain.n).to.equal("tRiARNa5Dj_L34A7caaTxah35ZNcEmFR1eGpRI0jJEAMeK40xTb5YPiIYXfGbhGQ4aVOgMHeL79u79ag0UhmsDoyZgoCxs4lJuAxZ4YGFqGLisbNNqdQFTJe1UG0EK12kDd7yXzhKDCE9KNWOqU61bhw5AocdAP6uATdYJD5Qn0");
+
+        });
+
+        it('generates new keys if there are no keys in the local storage', async () => {
+            //localStorage is a singleton, so let's make sure no keys are stored there
+            localStorage.removeItem('vault-public-key');
+            localStorage.removeItem('vault-private-key');
+
+            const keyChain = new KeychainService();
+            // All crypto operations are async, because they're computationally heavy and thus offloaded to a web worker
+            // Therefore we also need to create a ready method
+            await keyChain.ready();
+
+            const publicKey = localStorage.getItem('vault-public-key') || 'undefined';
+            const privateKey = localStorage.getItem('vault-private-key') || 'undefined';
+            expect(publicKey).not.to.equal('undefined');
+            expect(privateKey).not.to.equal('undefined');
+            expect(keyChain.n).to.equal(JSON.parse(publicKey).n);
+
+        });
+
+        it('clears malformed keys from local storage and replaces it with newly generated keys' +
+            ' adding a console warning', async () => {
+
+            // We add rubbish to the local storage
+            localStorage.setItem('vault-public-key', 'CORRUPTED KEY');
+            localStorage.setItem('vault-private-key', 'CORRUPTED KEY');
+
+            const warn = sinon.spy(console, 'warn');
+
+            const keyChain = new KeychainService();
+            await keyChain.ready();
+
+            expect(warn.calledWith('keys from local storage are not correct, generating new keys')).to.be.true;
+
+            const publicKey = localStorage.getItem('vault-public-key') || 'undefined';
+            const privateKey = localStorage.getItem('vault-private-key') || 'undefined';
+            expect(publicKey).not.to.equal('undefined');
+            expect(publicKey).not.to.equal('CORRUPTED KEY');
+            expect(privateKey).not.to.equal('undefined');
+            expect(privateKey).not.to.equal('CORRUPTED KEY');
+            expect(keyChain.n).to.equal(JSON.parse(publicKey).n);
+
+            warn.restore();
+
+        });
+
+
+    });
+
 });
--- src/keychain.service.ts
+++ src/keychain.service.ts
@@ -11,5 +11,79 @@
  *      or generating new keys and storing them in the local storage
  */
 
+import {localStorage} from "./mocks/local-storage.mock";
+
+// We are going to use a standard RSA-OAEP configuration for our encryption
+// Using the web crypto API (https://developer.mozilla.org/en-US/docs/Web/API/Web_Crypto_API)
+const RSA_CONFIG: RsaHashedKeyGenParams = {
+    name: 'RSA-OAEP',
+    modulusLength: 1024,
+    publicExponent: new Uint8Array([1, 0, 1]),
+    hash: 'SHA-256',
+};
+
 export class KeychainService {
+
+    public n: string | undefined; // n: The modulus value of the RSA key, Base64 URL encoded.
+    private _privateKey: CryptoKey | undefined;
+    private _publicKey: CryptoKey | undefined;
+    private initialized: boolean = false;
+
+    /**
+     * retrieving the keys from local storage
+     *      or generating new keys and storing them in the local storage
+     */
+    constructor() {
+        // A constructor cannot handle async operations, thus we need to pus that off to an initializer function
+        // all crypto methods are async because it is computationally heavy, therefore offloading it onto a web worker
+        // I like my initialize method to be private, so we need to test it through other methods
+        this.initialize().then(() => {
+            this.initialized = true;
+        });
+    }
+
+    private async initialize() {
+        const publicKey = localStorage.getItem('vault-public-key');
+        const privateKey = localStorage.getItem('vault-private-key');
+
+        if (publicKey && privateKey) {
+            try {
+                const jwkPublic = JSON.parse(publicKey);
+                this._privateKey = await crypto.subtle.importKey('jwk', JSON.parse(privateKey), RSA_CONFIG, true, ['decrypt']);
+                this._publicKey = await crypto.subtle.importKey('jwk', jwkPublic, RSA_CONFIG, true, ['encrypt']);
+                this.n = jwkPublic.n;
+            } catch (e: unknown) {
+                console.warn('keys from local storage are not correct, generating new keys');
+
+                // We just clear the keys
+                localStorage.removeItem('vault-public-key');
+                localStorage.removeItem('vault-private-key');
+
+                // and reinitialise
+                await this.initialize();
+            }
+        } else {
+            const {privateKey, publicKey} = await crypto.subtle.generateKey(RSA_CONFIG, true, ['decrypt', 'encrypt']);
+            this._publicKey = publicKey;
+            this._privateKey = privateKey;
+            const jwkPublic = await crypto.subtle.exportKey('jwk', publicKey);
+            const jwkPrivate = await crypto.subtle.exportKey('jwk', privateKey);
+            this.n = jwkPublic.n;
+            localStorage.setItem('vault-private-key', JSON.stringify(jwkPrivate));
+            localStorage.setItem('vault-public-key', JSON.stringify(jwkPublic));
+        }
+
+    }
+
+    public ready(): Promise<boolean> {
+        return new Promise((resolve) => {
+            const _ready = () => {
+                if (this.initialized) {
+                    resolve(true);
+                }
+                setTimeout(_ready, 10);
+            };
+            _ready();
+        });
+    }
 }
